34bb1b99bc6dfdc3e7bb860c5efbf4bea415fbd9
theinpu <theinpu@gmail.com>
2012-10-09 12:26:22 +0400

    добавили слим и твиг

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,4 +1,27 @@
 <?php
 
-echo "Hello, WORLD! I'm new awesome pron-site!";
-echo "test hui"
+$incPath = get_include_path();
+$rootPath = realpath(dirname(__FILE__).'/../');
+set_include_path($incPath.':'.$rootPath);
+
+require_once 'libs/Slim/Slim.php';
+require_once 'libs/TwigAutoloader.php';
+
+Twig_Extensions_Autoloader::register();
+
+require_once 'libs/TwigView.php';
+
+$slim = new Slim(array(
+                      'view'           => new TwigView(),
+                      'debug'          => true,
+                      'log.enable'     => false,
+                      'log.path'       => '../logs',
+                      'log.level'      => 4,
+                      'templates.path' => '../data/templates'
+                 ));
+
+$slim->get('/', function () use ($slim) {
+    echo 'PRON!!!';
+});
+
+$slim->run();
