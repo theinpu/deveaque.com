097864021bed8ed8117ab9660714f4521e7ad243
theinpu <theinpu@gmail.com>
2012-10-17 17:54:36 +0400

    просто сейв

--- app/pages/AuthHandler.php
+++ app/pages/AuthHandler.php
@@ -11,26 +11,27 @@ class AuthHandler extends Page {
 
     public function __construct($slim) {
         parent::__construct($slim);
-        $this->configFile = dirname(__FILE__) . '/../../htdocs/hybridauth/config.php';
+        $this->configFile = dirname(__FILE__).'/../../htdocs/hybridauth/config.php';
     }
 
     public function login() {
-        var_dump($_SESSION);
-        if (isset($_GET["login"])) {
+        if(isset($_GET["auth"])) {
             try {
                 $hybridauth = new Hybrid_Auth($this->configFile);
                 $this->adapter = $hybridauth->authenticate("Vkontakte");
                 $this->userProfile = $this->adapter->getUserProfile();
-            } catch (Exception $e) {
-                die("<b>got an error!</b> " . $e->getMessage());
+            }
+            catch(Exception $e) {
+                die("<b>got an error!</b> ".$e->getMessage());
             }
         }
-        if (is_null($this->userProfile)) {
+        if(is_null($this->userProfile)) {
             ?>
         <p>Нажмите Войти для авторизации.</p>
-        <h2><a href="/login?login=1">Войти</a></h2>
+        <h2><a href="/login?auth=1">Войти</a></h2>
         <?php
-        } else {
+        }
+        else {
             ?>
         <fieldset>
             <legend>twitter данные</legend>
