0b3c0cc9a2ea2b7c37afef673a9438e08b100017
theinpu <theinpu@gmail.com>
2012-10-17 17:59:00 +0400

    маленький помошник для мемкеша

---
+++ libs/MemcacheAssist.php
@@ -0,0 +1,33 @@
+<?php
+
+class MemcacheAssist {
+
+    /**
+     * @var Memcache
+     */
+    private static $mmc = null;
+
+    public static function getValue($key) {
+        self::connectToMemcache();
+
+        return self::$mmc->get($key);
+    }
+
+    public static function setValue($key, $value) {
+        self::connectToMemcache();
+        self::$mmc->set($key, $value);
+    }
+
+    public static function deleteValue($key) {
+        self::connectToMemcache();
+        self::$mmc->delete($key);
+    }
+
+    private static function connectToMemcache() {
+        if(is_null(self::$mmc)) {
+            self::$mmc = new Memcache();
+            self::$mmc->connect('localhost');
+        }
+    }
+
+}
