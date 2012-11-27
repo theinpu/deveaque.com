c3b3bf7795757822200d7396dbd46a73f4d591bc
theinpu <theinpu@gmail.com>
2012-11-27 15:08:00 +0400

    добавили интерфейс для юзверей, почти замутили регу

--- app/pages/RegisterHandler.php
+++ app/pages/RegisterHandler.php
@@ -6,4 +6,27 @@ class RegisterHandler extends Page {
         $this->displayTemplate('forms/register.twig');
     }
 
+    public function register() {
+        $email = $this->getSlim()->request()->post('email');
+        $pass = $this->getSlim()->request()->post('pass');
+        $pass1 = $this->getSlim()->request()->post('pass_second');
+
+        if(empty($email) || empty($pass) || empty($pass1)) {
+            $this->appendDataToTemplate(array('empty' => true));
+            $this->displayTemplate('forms/register.twig');
+
+            return;
+        }
+
+        if($pass != $pass1) {
+            $this->appendDataToTemplate(array('pass' => true));
+            $this->displayTemplate('forms/register.twig');
+
+            return;
+        }
+
+        Users::registerUser($email, $pass);
+        $this->getSlim()->redirect('/');
+    }
+
 }
