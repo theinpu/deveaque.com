d32d62339643bacc12d075774b65a9a9c0707854
theinpu <theinpu@gmail.com>
2012-11-27 23:49:59 +0400

    что-то натворили с серверной частью, кажись готово

--- app/models/Votes.php
+++ app/models/Votes.php
@@ -4,20 +4,43 @@ require_once 'libs/MongoAssist.php';
 
 class Votes {
 
+    /**
+     * @var MongoCollection
+     */
+    private static $votes = null;
+
     public static function getRating($postId) {
-        return 0;
+        self::checkCollecktion();
+        $count = self::$votes->count(array('post' => $postId, 'dir' => 'asc'));
+        $count -= self::$votes->count(array('post' => $postId, 'dir' => 'desc'));
+
+        return $count;
     }
 
     public static function rateUp($postId, $userId) {
-
+        self::checkCollecktion();
+        if(self::$votes->count(array('post' => $postId, 'user' => $userId)) == 0) {
+            self::$votes->insert(array('post' => $postId, 'user' => $userId, 'dir' => 'asc'));
+        }
     }
 
     public static function rateDown($postId, $userId) {
-
+        self::checkCollecktion();
+        if(self::$votes->count(array('post' => $postId, 'user' => $userId)) == 0) {
+            self::$votes->insert(array('post' => $postId, 'user' => $userId, 'dir' => 'desc'));
+        }
     }
 
     public static function isVoted($postId, $userId) {
-        return false;
+        self::checkCollecktion();
+
+        return self::$votes->count(array('post' => $postId, 'user' => $userId)) != 0;
+    }
+
+    private static function checkCollecktion() {
+        if(is_null(self::$votes)) {
+            self::$votes = MongoAssist::GetCollection('votes');
+        }
     }
 
 }
