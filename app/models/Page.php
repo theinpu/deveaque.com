f41332948b3b590c6971d1faedd9341c3bee9e30
theinpu <theinpu@gmail.com>
2012-12-05 00:23:44 +0400

    начали

--- app/models/Page.php
+++ app/models/Page.php
@@ -1,39 +1,5 @@
 <?php
 
-require_once 'PostFactory.php';
-require_once 'libs/MemcacheAssist.php';
-
-class Page {
-
-    const PostPerPage = 20;
-
-    private $posts = array();
-    /**
-     * @var MongoId
-     */
-    private $id = null;
-    private $pageNumber = 0;
-
-    public function __construct($id = null, $pageData = array()) {
-        $this->checkConstructParams($id, $pageData);
-    }
-
-    private function checkConstructParams($id, $pageData) {
-        if($id == null && empty($pageData)) {
-            throw new InvalidArgumentException();
-        }
-    }
-
-    public function addPost($postId) {
-
-    }
-
-    public function removePost($postId) {
-
-    }
-
-    public function getPosts() {
-
-    }
+interface Page {
 
 }
--- app/models/PageCollection.php
+++ app/models/PageCollection.php
@@ -1,35 +1,5 @@
 <?php
 
-require_once 'libs/MongoAssist.php';
-require_once 'Page.php';
-
 class PageCollection {
-
-    /**
-     * @var MongoCollection
-     */
-    private static $pages = null;
-
-    public static function getLastPage() {
-        self::checkCollection();
-        $pageData = self::$pages->findOne(array('posts' >= array('$lt' => Page::PostPerPage)));
-
-        return new Page(null, $pageData);
-    }
-
-    private static function checkCollection() {
-        if(is_null(self::$pages)) {
-            self::$pages = MongoAssist::GetCollection('pages');
-        }
-    }
-
-    public static function getPage($index) {
-        self::checkCollection();
-
-    }
-
-    public static function getPagesCount() {
-
-    }
 
 }
