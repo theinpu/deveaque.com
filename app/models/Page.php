2bd663887129e4d8191cad10060c4f0bea710f2a
theinpu <theinpu@gmail.com>
2012-12-05 17:55:22 +0400

    сделали метод для получение странички со свободным местод для постов

--- app/models/Page.php
+++ app/models/Page.php
@@ -1,17 +1,27 @@
 <?php
 
 require_once 'PostFactory.php';
+require_once 'libs/MemcacheAssist.php';
 
 class Page {
 
     const PostPerPage = 20;
 
     private $posts = array();
+    /**
+     * @var MongoId
+     */
     private $id = null;
     private $pageNumber = 0;
 
-    public function __construct($id = null) {
+    public function __construct($id = null, $pageData = array()) {
+        $this->checkConstructParams($id, $pageData);
+    }
 
+    private function checkConstructParams($id, $pageData) {
+        if($id == null && empty($pageData)) {
+            throw new InvalidArgumentException();
+        }
     }
 
     public function addPost($postId) {
