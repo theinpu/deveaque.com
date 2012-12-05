42958778c0c8c7d7ac989a3a5c9929cb689971fe
theinpu <theinpu@gmail.com>
2012-12-05 17:55:22 +0400

    сделали метод для получение странички со свободным местод для постов

--- app/models/PageCollection.php
+++ app/models/PageCollection.php
@@ -1,14 +1,30 @@
 <?php
 
+require_once 'libs/MongoAssist.php';
 require_once 'Page.php';
 
 class PageCollection {
 
-    public static function getLastPage() {
+    /**
+     * @var MongoCollection
+     */
+    private static $pages = null;
 
+    public static function getLastPage() {
+        self::checkCollection();
+        $pageData = self::$pages->findOne(array('posts' >= array('$lt' => Page::PostPerPage)));
+
+        return new Page(null, $pageData);
+    }
+
+    private static function checkCollection() {
+        if(is_null(self::$pages)) {
+            self::$pages = MongoAssist::GetCollection('pages');
+        }
     }
 
     public static function getPage($index) {
+        self::checkCollection();
 
     }
 
