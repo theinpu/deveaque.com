6959c021dca94793d3f98ed2afaea348136e16c6
id <id.backup@mail.ru>
2012-12-12 15:10:12 +0300

    test commit

--- startup.php
+++ startup.php
@@ -5,3 +5,6 @@ $_SERVER['DEVELOP'] = strpos($_SERVER['HTTP_HOST'], 'dev.') !== false;
 $incPath = get_include_path();
 $rootPath = realpath(dirname(__FILE__));
 set_include_path($rootPath.':'.$incPath);
+
+
+/* test commit*/
