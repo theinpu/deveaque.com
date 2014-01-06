84fdefb28e6318eb843af9eee9788489674e1982
inhuman <id.backup@mail.ru>
2014-01-06 21:26:12 +0400

    передача тегов через массив заработала, ура ура

--- app/pages/SandboxHandler.php
+++ app/pages/SandboxHandler.php
@@ -6,10 +6,13 @@ class SandboxHandler extends Page{
 
     public function showNewPics(){
 
-        $TagsArr=array(
-            'color' =>  ""
-        );
-        $this->getSlim()->render('sandbox/new.twig', array('path' => self::ShowLastPic(), 'TagsArr' => $TagsArr));
+        $TagsArr = array();
+        //$this->getSlim()->render('sandbox/new.twig', array('path' => self::ShowLastPic(), 'TagsArr' => $TagsArr));
+        $this->appendDataToTemplate(array('path' => self::ShowLastPic()));
+        $this->appendDataToTemplate(array('post' => $TagsArr));
+        $this->displayTemplate('sandbox/new.twig');
+
+
     }
 
     static public function ShowLastPic()
@@ -22,10 +25,11 @@ class SandboxHandler extends Page{
      public function PostPic()
     {
         //$color = $_POST['color'];
-        $TagsArr[] = $_POST['TagsArr[]'];
-
+      //   $TagsArr = $this->getSlim()->post('TagsArr');
+        $TagsArr = $this->getSlim()->request()->post("TagsArr");
 
         var_dump($TagsArr);
+
 
 
         //$this->getSlim()->redirect('/');
